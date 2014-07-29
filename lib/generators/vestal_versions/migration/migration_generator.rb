4e3ed17ef2e902a97e22b8a08dc42cc522aa60eb
Nitin Misra <nitin.misra23@gmail.com>
2014-07-30 01:17:06 +0530

    Added migration for request info and controller tests ::WIP::

--- lib/generators/vestal_versions/migration/migration_generator.rb
+++ lib/generators/vestal_versions/migration/migration_generator.rb
@@ -10,6 +10,7 @@ module VestalVersions
 
       def generate_files
         migration_template 'migration.rb', "db/migrate/#{name}.rb"
+        migration_template 'add_request_info_to_versions.rb', 'db/migrate/add_request_info_to_versions.rb'
         template 'initializer.rb', 'config/initializers/vestal_versions.rb'
       end
     end
