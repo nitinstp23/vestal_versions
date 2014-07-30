ce2635968749f61d816d8592bc1c5d81e87fba88
Nitin Misra <nitin.misra23@gmail.com>
2014-07-30 14:55:52 +0530

    Added dummy rails app for controller tests.

--- spec/vestal_versions/users_spec.rb
+++ spec/vestal_versions/users_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe VestalVersions::Users do
-  let(:updated_by){ User.create(:name => 'Steve Jobs') }
-  let(:user){ User.create(:name => 'Steve Richert') }
+
+  before do
+    User.destroy_all
+
+    @updated_by = User.create(:name => 'Steve Jobs')
+    @user = User.create(:name => 'Steve Richert')
+  end
 
   it 'defaults to nil' do
-    user.update_attributes(:first_name => 'Stephen')
-    user.versions.last.user.should be_nil
+    @user.update_attributes(:first_name => 'Stephen')
+    @user.versions.last.user.should be_nil
   end
 
   it 'accepts and returns an ActiveRecord user' do
-    user.update_attributes(:first_name => 'Stephen', :updated_by => updated_by)
-    user.versions.last.user.should == updated_by
+    @user.update_attributes(:first_name => 'Stephen', :updated_by => @updated_by)
+    @user.versions.last.user.should == @updated_by
   end
 
   it 'accepts and returns a string user name' do
-    user.update_attributes(:first_name => 'Stephen', :updated_by => updated_by.name)
-    user.versions.last.user.should == updated_by.name
+    @user.update_attributes(:first_name => 'Stephen', :updated_by => @updated_by.name)
+    @user.versions.last.user.should == @updated_by.name
   end
+
 end
